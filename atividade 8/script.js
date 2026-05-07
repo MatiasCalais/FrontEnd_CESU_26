606ab0f4a8411dfc32ddebf03dd6f90de20b6209
Mateus Henrique <mateus12calais.costa@gmail.com>
2026-05-07 11:42:31 -0300

    Enhance task management with edit, remove, and complete buttons
    Added buttons for editing, removing, and completing tasks in the task list.

--- atividade 8/script.js
+++ atividade 8/script.js
@@ -1,5 +1,5 @@
 const  taskinput = document.getElementById('taskInput'); //getbyide serve basicamente para buscar em HTML
-const taskList = document.getElementById('taskList');//mesma coisa inclusive os IDs se tornão essas costancias
+const taskList = document.getElementById('taskList');//mesma coisa inclusive os IDs se tornãogf essas costancias
 
 //isso e colocado para que seja 'declarado ' as costantes do HTML antes de codar para ser editado.
 //================================================
@@ -7,7 +7,23 @@ function addTask(){
  const textInput = taskInput.value.trim();
  if(textInput !== ''){
   const newTask = document.createElement('li'); //esta criando dentro do 
-  newTask.innerHTML =   `<span>${textInput}<span>`; //newtack esta colocaando dentro do newtask
+  newTask.innerHTML =   `<span>${textInput}<span>
+                        <button onclick="editar()">Editar</burron>
+                        <button onclick="remover(this)">Remover</button>
+                        <button onclick="conclui(this)">Concluir</button>           `; //newtack esta colocaando dentro do newtask
   taskList.appendChild(newTask);
  }
+}
+
+function remover(button){
+  
+  const itemToRemove = button.parentElement;
+  taskList.removeChild(itemToRemove);
+}
+
+function concluir(button){
+  
+  const DeBom = button.parentElement;
+  DeBom.classList.toggle('completed');
+  
 }
