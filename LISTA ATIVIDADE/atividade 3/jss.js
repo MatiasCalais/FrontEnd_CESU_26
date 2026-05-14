8686aeb6683bb52936107757789445ee0e4aaa00
Mateus Henrique <mateus12calais.costa@gmail.com>
2026-05-14 11:27:49 -0300

    Add files via upload

---
+++ LISTA ATIVIDADE/atividade 3/jss.js
@@ -0,0 +1,4 @@
+const  NT1 = document.getElementById('nota1');
+const  NT2 = document.getElementById('nota2');
+const  RCP = document.getElementById('recPe');
+const  Conv = document.getElementById('');
